eed84f6c75e7504319d2c2081a30cac758be053c
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-20 08:48:16 -0700

    Assembly Endgame: Status Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Status from './components/Status'
 
 
 export default function AssemblyEndgame() {
   return (
     <main>
       <Header />
+      <Status />
     </main>
     
   )
---
+++ src/components/Status.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export default function Status() {
+  return (
+    <section className="game-status">
+      <h2>You Win!</h2>
+      <p>Well Done! 🎉</p>
+    </section>
+    
+
+  )
+}
