4ea2a68d0d7bd5a5bbb5b2ee3d64dc705a75a1de
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-24 12:09:15 -0700

    Assembly Endgame: Conditional rendering with a helper function

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -5,24 +5,31 @@ export default function Status(props) {
     won: props.isGameWon,
     lost: props.isGameLost
   })
+
+  function renderGameStatus() {
+    if (!props.isGameOver) {
+      return null
+    }
+
+    if (props.isGameWon) {
+      return (
+        <>
+          <h2>You Win!</h2>
+          <p>Well Done! 🎉</p>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <h2>You Lose</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      )
+    }
+  }
   return (
     <section className={gameStatusClass}>
-      {props.isGameOver ? (
-        props.isGameWon ? (
-          <>
-            <h2>You Win!</h2>
-            <p>Well Done! 🎉</p>
-          </>
-        ) : (
-          <>
-            <h2>You Lose</h2>
-            <p>You lose! Better start learning Assembly 😭</p>
-          </>
-          )
-      ) : (
-          null
-        )
-      }
-</section>
+      {renderGameStatus()}
+    </section>
   )
 }
